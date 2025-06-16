e109cc8ef38a4a4771eef0133f1dd5126a55d6fb
tejaswi-ramineni <tejaswi.ramineni81@gmail.com>
2025-06-16 10:14:36 +0530

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware to handle CORS
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "*",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
