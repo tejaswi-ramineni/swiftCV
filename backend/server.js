ea451ea4a1ac9f358c36082484345df1525e0361
tejaswi-ramineni <tejaswi.ramineni81@gmail.com>
2025-06-16 10:33:26 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware to handle CORS
 app.use(
   cors({
-    origin: "*",
+    origin: process.env.CLIENT_URL || "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
